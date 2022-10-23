76b0d9e4e08caca34f59883a9195589d716cee52
Stanislav-PC <stasholoborodyi@gmail.com>
2022-10-24 01:02:48 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import './css/tabler.min.css'
 import Header from "./components/Header/Header";
 import TopicList from "./components/TopicList/TopicList";
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App theme-dark">
       <Header/>
-        <TopicList/>
+      <Routes>
+        <Route path="/" element={<TopicList />}>
+          {/*<Route index element={<TopicList/>} />*/}
+          {/*<Route path="teams" element={<Teams />}>*/}
+          {/*<Route path=":teamId" element={<Team />} />*/}
+          {/*<Route path="new" element={<NewTeamForm />} />*/}
+          {/*<Route index element={<LeagueStandings />} />*/}
+          {/*</Route>*/}
+        </Route>
+        {/*<Route path="/details/:topicId" element={<TopicDetails/>}/>*/}
+      </Routes>
     </div>
   );
 }
