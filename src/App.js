ebd5dc02e28484e4ba85a337a0832279696d2d12
Mykhailo_Olefirenko <mihailolefirenko.2002@gmail.com>
2022-10-25 14:43:41 +0300

    SignIn, SignUp form added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import './css/tabler.min.css'
 import Header from "./components/Header/Header";
 import TopicList from "./components/TopicList/TopicList";
+import RegistrationDataInput from "./components/RegistrationForm/RegistrationDataInput";
+import SignIn from "./components/LoginForm/SignIn";
+import SignUp from "./components/RegistrationForm/SignUp";
 
 function App() {
   return (
     <div className="App theme-dark">
-      <Header/>
-        <TopicList/>
+      {/*<Header/>*/}
+        {/*<TopicList/>*/}
+        <SignUp/>
     </div>
   );
 }
